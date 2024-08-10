f0d56f0a9060ee7c6420af479e524459859099fa
bigTogu <silvia.kenaan@gmail.com>
2024-08-10 12:26:48 +0200

    feat: Widget component

---
+++ catalog/src/components/Widget.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Widget() {
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Me llamo Jhon</div>
+      <div>CSS: Tailwind</div>
+    </div>
+  );
+}
