afb883bc46f62ec810dbbff3360a0993797493ea
bigTogu <silvia.kenaan@gmail.com>
2024-08-11 19:54:42 +0200

    feat: Error Boundary and lazy loading for Widget component

--- host/src/App.jsx
+++ host/src/App.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import Widget from "catalog/Widget";
 
 import "./index.scss";
+import SafeComponent from "./SafeComponent";
 
+const Widget = React.lazy(() => import("catalog/Widget"));
 const App = () => (
   <div className="bg-red-500 w-full h-full">
-    <Widget />
+    <SafeComponent>
+      <Suspense>
+        <Widget />
+      </Suspense>
+    </SafeComponent>
   </div>
 );
 const rootElement = document.getElementById("app");
