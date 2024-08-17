b48c28456fb1507de769fc3fbbf5a7ebd32bddf7
bigTogu <silvia.kenaan@gmail.com>
2024-08-17 12:36:51 +0200

    chore: comment some sections that i will use in the future

--- host/src/MainLayout.jsx
+++ host/src/MainLayout.jsx
@@ -5,19 +5,31 @@ import "remixicon/fonts/remixicon.css";
 import PDPContent from "pdp/ProductContent";
 import HomeContent from "catalog/HomeContent";
 import CartContent from "cart/CartContent";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
+import NotFound from "./pages/NotFound";
+import UploadFile from "./pages/UploadFile";
 
-export function MainLayout() {
+import Home from "./pages/Home";
+export function MainLayout({ children }) {
   return (
     <Router>
       <NavBar />
-      <div className="container mx-auto mb-4">
-        <Routes>
-          <Route exact path="/" Component={HomeContent} />
-          <Route path="/products/:id" Component={PDPContent} />
-          <Route path="/cart" Component={CartContent} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route exact path="/" Component={HomeContent} />
+        <Route path="/products/:id" Component={PDPContent} />
+        <Route path="/cart" Component={CartContent} />
+        <Route path="/404" element={<NotFound />} />
+      </Routes>
     </Router>
+
+    // <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+    //   <div className="flex-1 flex flex-col h-full">
+    //     <Router>
+    //       <Routes>
+    //         <Route exact path="/" Component={UploadFile} />
+    //       </Routes>
+    //     </Router>
+    //   </div>
+    // </main>
   );
 }
