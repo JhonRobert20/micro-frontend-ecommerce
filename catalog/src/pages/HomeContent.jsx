c9895e0b59fb7fa899d99e790b0b007cc913b8f3
bigTogu <silvia.kenaan@gmail.com>
2024-08-19 19:36:06 +0200

    refactor HomeContent with new components

--- catalog/src/pages/HomeContent.jsx
+++ catalog/src/pages/HomeContent.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-
+import Card from "../components/Card";
 import { getProducts, getProductById } from "../data/products";
-import { StoreProduct } from "../components/StoreProduct";
+import CarouselCards from "../components/CarouselCards";
+import { Carousel } from "../components/Carousel";
 export default function HomeContent() {
   const [products, setProducts] = useState([]);
 
@@ -11,18 +12,9 @@ export default function HomeContent() {
 
   return (
     <>
-      <div className="my-10 flex justify-center">
-        <div className="grid grid-cols-3 gap-1">
-          {products.map((product) => (
-            <article
-              key={product.id}
-              className="flex flex-col max-w-[300px] bg-[#F5EEE3] hover:bg-[#F7F3ED] hover:shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)] p-6"
-            >
-              <StoreProduct {...product} />
-            </article>
-          ))}
-        </div>
-      </div>
+      <Card />
+      <CarouselCards />
+      {/* <Carousel autoPlay={true} showBtns={false} /> */}
     </>
   );
 }
