42f35a5906def1992c4489d873101aa85bce484b
bigTogu <silvia.kenaan@gmail.com>
2024-08-21 17:42:41 +0200

    add: Component Modal reusable

---
+++ cart/src/components/UI/Modal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import { motion } from "framer-motion";
+
+const Modal = ({
+  classNameModal,
+  trigger,
+  contentClassName,
+  title,
+  content,
+  footerContent,
+  modalClose,
+  modalAnimation,
+}) => (
+  <Dialog.Root>
+    <Dialog.Trigger>{trigger}</Dialog.Trigger>
+    <Dialog.Portal className={classNameModal}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3 }}
+        exit={{ opacity: 0, transition: { delay: 0.2 } }}
+      >
+        <Dialog.Overlay className="fixed inset-0 bg-black/25" />
+      </motion.div>
+      <Dialog.Content asChild>
+        <motion.div {...modalAnimation} className={contentClassName}>
+          <section>
+            <Dialog.Title asChild>{title}</Dialog.Title>
+            <Dialog.Description asChild>{content}</Dialog.Description>
+          </section>
+          <Dialog.Close>{footerContent}</Dialog.Close>
+          <Dialog.Close>{modalClose}</Dialog.Close>
+        </motion.div>
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
+
+export default Modal;
