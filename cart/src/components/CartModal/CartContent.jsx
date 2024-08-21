83c601fe01d9ef7cbd20f62c305c103eac53723e
bigTogu <silvia.kenaan@gmail.com>
2024-08-21 19:56:50 +0200

    refactor: modal cart to add functionality

--- cart/src/components/CartModal/CartContent.jsx
+++ cart/src/components/CartModal/CartContent.jsx
@@ -6,40 +6,43 @@ import { Trash2, Minus, Plus } from "lucide-react";
 import { useShoppingCart } from "../../hooks/hooks";
 
 const CartContent = () => {
-  const { increaseCartQuantity, decreaseCartQuantity, cartQuantity, items } =
+  const { increaseCartQuantity, decreaseCartQuantity, removeQuantity, items } =
     useShoppingCart();
 
   return (
     <section className="flex flex-col items-start gap-6 text-base my-8">
-      {ITEMS.map((item, idx) => (
+      {items.map((item, idx) => (
         <motion.article
           key={idx}
           className="flex gap-6 items-center"
           {...textAnimation(idx)}
         >
           <img
-            src={item.src}
+            src={item.image}
             className="w-28 aspect-square flex-shrink-0 h-auto "
-            alt={item.title}
+            alt={item.name}
           />
           <div className="flex flex-col items-start gap-2">
             <div className="flex flex-col items-start gap-1">
-              <span>{item.title}</span>
+              <span>{item.name}</span>
               <span className="opacity-40 text-sm">{item.price}</span>
             </div>
             <div className="flex gap-4 items-center">
-              <div className="flex items-center  border-[1px] border-[#1C1C1C] opacity-20 rounded-sm">
+              <div className="flex items-center  border-[1px] border-[#1c1c1c44] rounded-sm">
                 <Minus
-                  onClick={decreaseCartQuantity}
-                  className="cursor-pointer aspect-square w-4 mx-2 my-1.5"
+                  onClick={() => decreaseCartQuantity(item.id)}
+                  className="cursor-pointer aspect-square w-4 mx-2 my-1.5 opacity-20 hover:opacity-100"
                 />
-                <span className="text-base">{cartQuantity}</span>
+                <span className="text-base opacity-20">{item.quantity}</span>
                 <Plus
-                  onClick={increaseCartQuantity}
-                  className="cursor-pointer aspect-square w-4 mx-2 my-1.5"
+                  onClick={() => increaseCartQuantity(item.id)}
+                  className="cursor-pointer aspect-square w-4 mx-2 my-1.5 opacity-20 hover:opacity-100"
                 />
               </div>
-              <Trash2 className="hover:text-[#f7f3ed] aspect-square w-5 cursor-pointer" />
+              <Trash2
+                onClick={() => removeQuantity(item.id)}
+                className="hover:text-[#f7f3ed] aspect-square w-5 cursor-pointer"
+              />
             </div>
           </div>
         </motion.article>
