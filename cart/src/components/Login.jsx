8235254695ffc9d0b1fc14d44be617d90e4d8e3a
bigTogu <silvia.kenaan@gmail.com>
2024-08-21 19:56:39 +0200

    refactor: login as before because of functionality

--- cart/src/components/Login.jsx
+++ cart/src/components/Login.jsx
@@ -1,21 +1,85 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { User } from "lucide-react";
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import { User } from "lucide-react";
 
-import { useLoggedIn } from "../cart";
-import IconNavBtn from "./UI/buttonNav";
+// import { useLoggedIn } from "../cart";
+// import IconNavBtn from "./UI/buttonNav";
+
+// export default function Login() {
+//   const loggedIn = useLoggedIn();
+//   const navigate = useNavigate();
+
+//   if (loggedIn) {
+//     return null;
+//   }
+
+//   return (
+//     <IconNavBtn onClick={() => navigate("/login")} ariaLabel="User Login">
+//       <User className="text-[#1C1C1C]" />
+//     </IconNavBtn>
+//   );
+// }
+
+import React, { useState } from "react";
+
+import { login, useLoggedIn } from "../cart";
+import { UserRound } from "lucide-react";
 
 export default function Login() {
   const loggedIn = useLoggedIn();
-  const navigate = useNavigate();
+  const [showLogin, setShowLogin] = useState(false);
 
-  if (loggedIn) {
-    return null;
-  }
+  const [username, setUsername] = useState("sally");
+  const [password, setPassword] = useState("123");
+
+  if (loggedIn) return null;
 
   return (
-    <IconNavBtn onClick={() => navigate("/login")} ariaLabel="User Login">
-      <User className="text-[#1C1C1C]" />
-    </IconNavBtn>
+    <>
+      <button
+        type="button"
+        className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+        id="user-menu-button"
+        aria-expanded="false"
+        aria-haspopup="true"
+        onClick={() => setShowLogin(!showLogin)}
+      >
+        <span className="absolute -inset-1.5"></span>
+        <span className="sr-only">Open user menu</span>
+        <UserRound />
+      </button>
+
+      {showLogin && (
+        <div
+          className="absolute p-5 border-4 border-blue-800 bg-white rounded-xl text-black"
+          style={{
+            width: 300,
+            top: "2rem",
+            // left: -250,
+          }}
+        >
+          <input
+            type="text"
+            placeholder="User Name"
+            value={username}
+            onChange={(evt) => setUsername(evt.target.value)}
+            className="border text-sm border-gray-400 p-2 rounded-md w-full"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
+            className="border text-sm border-gray-400 p-2 rounded-md w-full mt-3"
+          />
+          <button
+            className="bg-green-900 text-white py-2 px-5 rounded-md text-sm mt-5"
+            onClick={() => login(username, password)}
+            id="loginbtn"
+          >
+            Login
+          </button>
+        </div>
+      )}
+    </>
   );
 }
