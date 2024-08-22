9dc271782db3652065ac80697f8f08b9cad734ff
bigTogu <silvia.kenaan@gmail.com>
2024-08-22 17:30:24 +0200

    add checkout in tailwind host to see checkout styles

--- host/tailwind.config.js
+++ host/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     join(__dirname, "../cart/src/**/*.{js,jsx,ts,tsx,css,scss}"),
     join(__dirname, "../catalog/src/**/*.{js,jsx,ts,tsx,css,scss}"),
+    join(__dirname, "../checkout/src/**/*.{js,jsx,ts,tsx,css,scss}"),
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
