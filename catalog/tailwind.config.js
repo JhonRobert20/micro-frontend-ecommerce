453b80fcbeb521f743865cba2513add32008e78d
bigTogu <silvia.kenaan@gmail.com>
2024-08-29 23:04:39 +0200

    add: theme colors

--- catalog/tailwind.config.js
+++ catalog/tailwind.config.js
@@ -2,10 +2,34 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          base: "var(--color-text-base)",
+          "base-hover": "var(--color-text-base-hover)",
+          details: "var(--color-details)",
+          decorative: "var(--color-decorative-section)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-primary)",
+          "button-acent": "var(--color-button-acent)",
+          "button-acent-hover": "var(--color-button-acent-hover)",
+          decorative: "var(--color-decorative-section)",
+          details: "var(--color-details)",
+        },
+      },
+      borderColor: {
+        skin: {
+          details: "var(--color-details)",
+          decorative: "var(--color-decorative-section)",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
