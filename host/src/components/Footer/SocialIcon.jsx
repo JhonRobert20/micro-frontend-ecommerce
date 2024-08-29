15197d9ad31bb1a26341425dd75e64c4bb713c5b
bigTogu <silvia.kenaan@gmail.com>
2024-08-29 20:16:47 +0200

    refactor: css transition duration to social icons in footer

--- host/src/components/Footer/SocialIcon.jsx
+++ host/src/components/Footer/SocialIcon.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SocialIcon = ({ Icon, link }) => (
   <a
     href={link}
-    className="w-9 h-9 rounded-sm bg-skin-details flex justify-center items-center hover:bg-skin-decorative text-skin-base hover:text-skin-base-hover"
+    className="w-9 h-9 rounded-sm bg-skin-details flex justify-center items-center hover:bg-skin-decorative text-skin-base hover:text-skin-base-hover transition-colors duration-200"
     aria-label={Icon.displayName || "social-icon"}
   >
     <Icon aria-hidden={link} />
