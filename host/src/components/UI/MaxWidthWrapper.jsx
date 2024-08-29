55ea45b42d32200aa70a723247561e146dbb6e22
bigTogu <silvia.kenaan@gmail.com>
2024-08-29 20:14:48 +0200

    refactor: MaxWidthWrapper

--- host/src/components/UI/MaxWidthWrapper.jsx
+++ host/src/components/UI/MaxWidthWrapper.jsx
@@ -2,7 +2,7 @@ import { cn } from "../../lib/utils";
 
 const MaxWidthWrapper = ({ className, children }) => {
   return (
-    <div className={cn("h-full mx-auto w-full px-2.5 md:px-20", className)}>
+    <div className={cn(" mx-auto w-full px-4 md:px-8", className)}>
       {children}
     </div>
   );
