2113a5ea00812bca93769c59d2e80d3091e8ea4f
bigTogu <silvia.kenaan@gmail.com>
2024-08-29 23:04:57 +0200

    delete unnecessary files

--- host/src/components/HeaderSection.jsx
+++ host/src/components/HeaderSection.jsx
@@ -11,7 +11,7 @@ export default function HeaderSection({
   return (
     <div className={cn("flex flex-col items-center", className)}>
       <span className="relative px-2">{subtitleHeader}</span>
-      <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+      <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl">
         {titleHeader}
         <strong className=" font-bold">{strongTitle}</strong>
       </h2>
